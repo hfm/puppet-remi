afc26bd42314ddb37606306952c4dfd2dfb52a89
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-03-24 07:33:15 +0900

    Write class document

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,34 +6,19 @@
 # Parameters
 # ----------
 #
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
 # * `ensure`
-# Whether the RPM-GPG-KEY-remi file should exist.
+# Whether Remi's repositories and the RPM-GPG-KEY-remi file should exist.
 #
 # * `path`
 # The path to the RPM-GPG-KEY-remi file to manage. Must be an absolute path.
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
 #
 # Examples
 # --------
 #
 # @example
 #    class { 'remi':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#      remi_safe_enabled  => 1,
+#      remi_php70_enabled => 1,
 #    }
 #
 class remi (
