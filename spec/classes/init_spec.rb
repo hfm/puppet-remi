e76daede6dcc61a8835241ef2ee034cd96ca10ec
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-03-24 06:24:32 +0900

    Add compile tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 describe 'remi' do
 
   context 'with default values for all parameters' do
+    it { should compile }
+    it { should compile.with_all_deps }
     it { should contain_class('remi') }
   end
 end
